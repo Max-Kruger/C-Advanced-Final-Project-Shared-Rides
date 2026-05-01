ce92bb19dea87ef93424077ac77845e2c71bc06b
Max-Kruger <maxkruger202020@gmail.com>
2026-05-01 11:00:05 -0500

    The base project

---
+++ C# Advanced Final Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace C__Advanced_Final_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
