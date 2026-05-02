af04ed2df966892e8119b8b47791b95a40414527
Max-Kruger <maxkruger202020@gmail.com>
2026-05-01 19:43:10 -0500

    First install of Models

--- C# Advanced Final Project/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace C__Advanced_Final_Project.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
