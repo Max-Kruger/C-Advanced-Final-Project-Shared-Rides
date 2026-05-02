a82be076dad12b661d43d61eae85b45316081fe3
N-Rock <nvschoenrock@gmail.com>
2026-05-02 14:26:04 -0500

    5/2/2026

--- C# Advanced Final Project/obj/Debug/net8.0/C# Advanced Final Project.AssemblyInfo.cs
+++ C# Advanced Final Project/obj/Debug/net8.0/C# Advanced Final Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C# Advanced Final Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce92bb19dea87ef93424077ac77845e2c71bc06b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed3c36709fadc7b3e832ac5abfa98bea90f8a551")]
 [assembly: System.Reflection.AssemblyProductAttribute("C# Advanced Final Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C# Advanced Final Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
