332d0a9d753a01d985480e68037345943a8e8c4d
N-Rock <nvschoenrock@gmail.com>
2026-05-02 13:50:00 -0500

    Reapply "Added a Basic User: identity User class. Drivers and guests now have a User Assigned data member."
    This reverts commit 6d9c91cdc060618816e1953287da2a9e883896c4.

--- C# Advanced Final Project/Models/Driver.cs
+++ C# Advanced Final Project/Models/Driver.cs
@@ -1,15 +1,18 @@
-﻿namespace C__Advanced_Final_Project.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace C__Advanced_Final_Project.Models
 {
     public class Driver
     {
         public int DriverID { get; set; }
 
-        public string DriverName { get; set; } = string.Empty;
-
-        public string PhoneNumber { get; set; } = string.Empty;
+        public User driverUser { get; set; }
 
         public int CurrentPassengers {  get; set; }
 
         public int MaxCapacity  { get; set; }
+
+        [ValidateNever]
+        public int EventID { get; set; }
     }
 }
--- C# Advanced Final Project/Models/Guest.cs
+++ C# Advanced Final Project/Models/Guest.cs
@@ -5,12 +5,14 @@ namespace C__Advanced_Final_Project.Models
     public class Guest
     {
         public int Id { get; set; }
-        public string FName { get; set; }
 
-        public string LName { get; set; }
+        public User guestUser { get; set; }
 
         public string Address { get; set; }
+
+
         [Required (ErrorMessage = "Please enter an event")]
+
         public Event AttendingEvent { get; set; }
 
     }
